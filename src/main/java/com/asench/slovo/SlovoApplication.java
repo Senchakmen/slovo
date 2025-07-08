077e016827a0237d45acc9be56a1670460d03532
Senchakmen <senchakevuch@gmail.com>
2025-07-08 18:33:58 +0300

    Initial commit: create Spring Boot project

---
+++ src/main/java/com/asench/slovo/SlovoApplication.java
@@ -0,0 +1,13 @@
+package com.asench.slovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlovoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asench/slovo/SlovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asench.slovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
