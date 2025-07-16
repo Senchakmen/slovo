6dd2fa37e60c1fa14b3973efdc79bfcedb40a575
Senchakmen <senchakevuch@gmail.com>
2025-07-16 18:36:43 +0300

    add UserLanguagePair slice test for index constraint

--- src/test/java/com/asench/slovo/repository/UserLanguagePairRepositoryTest.java
+++ src/test/java/com/asench/slovo/repository/UserLanguagePairRepositoryTest.java
@@ -4,11 +4,11 @@ package com.asench.slovo.repository;
 import com.asench.slovo.domain.Language;
 import com.asench.slovo.domain.AppUser;
 import com.asench.slovo.domain.UserLanguagePair;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 
 public class UserLanguagePairRepositoryTest extends RepositoryTestBase {
     @Autowired
@@ -20,8 +20,6 @@ public class UserLanguagePairRepositoryTest extends RepositoryTestBase {
 
     private static final String BASE_SOURCE_LANGUAGE_CODE = "en";
     private static final String BASE_TARGET_LANGUAGE_CODE = "uk";
-    //private static final String BASE_USERNAME = "test";
-    //private static final String BASE_USER_EMAIL = "test@test.com";
 
     private AppUser baseAppUser;
     private Language languageSource;
@@ -29,16 +27,12 @@ public class UserLanguagePairRepositoryTest extends RepositoryTestBase {
 
     @BeforeEach
     void setup() {
-        baseAppUser = new AppUser(AppUserRepositoryTest.BASE_USERNAME, AppUserRepositoryTest.BASE_USER_EMAIL);
-        userRepo.save(baseAppUser);
+        if (!userRepo.existsByUsername(AppUserRepositoryTest.BASE_USERNAME)) {
+            baseAppUser = new AppUser(AppUserRepositoryTest.BASE_USERNAME, AppUserRepositoryTest.BASE_USER_EMAIL);
+            userRepo.save(baseAppUser);
+        }
         languageSource = langRepo.findByCodeIgnoreCase(BASE_SOURCE_LANGUAGE_CODE).orElseThrow();
         languageTarget = langRepo.findByCodeIgnoreCase(BASE_TARGET_LANGUAGE_CODE).orElseThrow();
-    }
-
-    @AfterEach
-    void tearDown() {
-        userRepo.findByUsername(AppUserRepositoryTest.BASE_USERNAME)
-                .ifPresent(appUser -> userRepo.delete(appUser));
     }
 
     @Test
@@ -56,6 +50,17 @@ public class UserLanguagePairRepositoryTest extends RepositoryTestBase {
 
         Assertions.assertTrue(userRepo.existsByUsername(AppUserRepositoryTest.BASE_USERNAME), "ifDeletePairUserShouldNotBeDeleted");
         Assertions.assertFalse(pairRepo.existsById(resultID));
+    }
+
+    @Test
+    void whenMoreThanOneLanguagePairIsDefaultForAppUserThrowsError() {
+        var userLanguagePair = new UserLanguagePair(baseAppUser, languageSource, languageTarget, true);
+        pairRepo.save(userLanguagePair);
+
+        Language languageDe = langRepo.findByCodeIgnoreCase("de").orElseThrow();
+        Assertions.assertThrows(DataIntegrityViolationException.class,
+                () -> pairRepo.save(new UserLanguagePair(baseAppUser, languageDe, languageTarget, true)),
+                "Each user should not able to flag more than one default pair");
     }
 
 }
